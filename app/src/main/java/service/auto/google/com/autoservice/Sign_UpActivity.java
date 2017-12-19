7dcd8464307b36b1d65f54211418daea7220428e
tio <dev.tio.saputra@gmail.com>
2017-12-19 16:11:25 +0700

    Initial commit

---
+++ app/src/main/java/service/auto/google/com/autoservice/Sign_UpActivity.java
@@ -0,0 +1,19 @@
+package service.auto.google.com.autoservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Sign_UpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign__up);
+    }
+
+    public void keMana(View view) {
+//        Intent i = new Integer(Sign_UpActivity.this, LoginActivity.class);
+//        startActivity(i);
+    }
+}
